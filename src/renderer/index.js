b0f74ffc14eecceb259c5b302cf170cce843d3b1
Atul <atulanand94@gmail.com>
2018-10-21 00:35:23 +0200

    Added renderer boilerplate

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,26 @@
+const Reconciler = require("react-reconciler");
+
+const HostConfig = {
+  //TODO We will specify all required methods here
+};
+const reconcilerInstance = Reconciler(HostConfig);
+
 const CustomRenderer = {
   render(element, renderDom, callback) {
     // element: This is the react element for App component
     // renderDom: This is the host root element to which the rendered app will be attached.
     // callback: if specified will be called after render is done.
-    console.log("render called", element, renderDom, callback);
+
+    const isAsync = false; // Disables async rendering
+    const container = reconcilerInstance.createContainer(renderDom, isAsync); // Creates root fiber node.
+
+    const parentComponent = null; // Since there is no parent (since this is the root fiber). We set parentComponent to null.
+    reconcilerInstance.updateContainer(
+      element,
+      container,
+      parentComponent,
+      callback
+    ); // Start reconcilation and render the result
   }
 };
 
